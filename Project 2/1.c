c3cbf8f2f4ea0800d56c425cd5a4c0c9bcb8df0b
Jotaniya-Darshit <jotaniyadarshit1235@gmail.com>
2024-05-03 16:45:50 +0530

    Project 2

--- Project 2/1.c
+++ Project 2/1.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 main() 
-{
+{   
+    int n;
     int a = 1;
+    printf("Enter Number = ");
+    scanf("%d",&n);
     do {
         if (a % 2 == 0) {
             printf("%d\n", a);
         }
         a += 3;
-    } while (a <= 50);
+    } while (a <= n);
 
 }
